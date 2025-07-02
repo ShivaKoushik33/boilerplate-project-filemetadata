344d84d37cc87f6706b85cce4c1bb06da277afdd
ShivaKoushik33 <shivakoushik7890@gmail.com>
2025-07-02 21:14:05 +0530

    changes made

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer= require('multer');
 var app = express();
-
+const  upload=multer();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +11,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=> {
+  res.json({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
